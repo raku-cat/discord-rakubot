0265fc24d0407bff7632a31186ef3f287e03e0f7
Raku-cat <raku@rakutiki.tv>
2017-03-29 18:58:21 -0500

    Cleaner

--- bot.py
+++ bot.py
@@ -31,17 +31,23 @@ async def on_message(message):
                 await client.send_message(message.channel, str(r.json()['response']['message'].rsplit('.', 2)[0]) + '.')
             else:
                 with open (sys.path[0] + '/bplist.json', 'w') as f:
-                    json.dump(r.json(), f)
+                    json.dump(r.json(), f, indent=4)
                     await client.send_message(message.channel, 'Item price list updated')
         else:
             with open(sys.path[0] + '/bplist.json', 'r') as f:
                 bpjs = json.load(f)
             try:
-                ipr = str(bpjs['response']['items'][itemq]['prices']['6']['Tradable']['Craftable'][0]['value'])
-                cur = str(bpjs['response']['items'][itemq]['prices']['6']['Tradable']['Craftable'][0]['currency'])
-                await client.send_message(message.channel, itemq + ' is currently priced at ' + ipr + ' ' + cur + '.')
+                itemobj = bpjs['response']['items'][itemq]['prices']['6']['Tradable']
+                ipr = str(itemobj['Craftable'][0]['value'])
+                cur = str(itemobj['Craftable'][0]['currency'])
             except KeyError:
-                await client.send_message(message.channel, 'Item not found')
+                try:
+                    ipr = str(itemobj['Non-Craftable'][0]['value'])
+                    cur = str(itemobj['Non-Craftable'][0]['currency'])
+                except (KeyError, UnboundLocalError):
+                    await client.send_message(message.channel, 'Item not found')
+                    return
+            await client.send_message(message.channel, itemq + ' is currently priced at ' + ipr + ' ' + cur + '.')
     else:
         pass
 
